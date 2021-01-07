730a7014bf4c325bece41229f774051ff067858e
Briac Ferte <briac.ferte@gmail.com>
2021-01-07 13:43:21 +0100

    first commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import { Button, Alert } from 'reactstrap'
+
+
+export default () => {
+    return (
+        <div>
+            <Alert color='primary'>
+                Super Projet avec NextJS et Bootstrap
+            </Alert>
+            &nbsp; <Button color='primary' >Hello from nextJS</Button>
+        </div>
+    )
+}
+
---
+++ pages/login.js
@@ -0,0 +1,3 @@
+export default () => {
+    return <h1>Se connecter</h1>
+}
